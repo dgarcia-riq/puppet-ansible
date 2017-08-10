537ae51633414f7b55a45578df4e86ec4c0ccea4
Edgar Silva <edgarsilva948@gmail.com>
2017-08-10 16:41:35 -0300

    Fix parameters install.pp

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,12 +1,22 @@
 ## Class to install core packages and ansible
 class ansible::install {
-  $rhel_packages = ['epel-release','python','python34','python-pip']
-
-  package { $rhel_packages:
+#  $rhel_packages = [ 'epel-release','python','PyYAML','libtomcrypt','libtommath','libyaml','python-babel','python-httplib2','python-jinja2','python-keyczar','python-markupsafe','python-six','python2-crypto','python2-ecdsa','python2-paramiko','gcc', 'gmp-devel','sshpass', 'python2-pyasn1','python34','python-pip']
+#  package { $rhel_packages:
+#    ensure => 'latest',
+#  }
+#  -> package { 'ansible':
+#    ensure      => 'present',
+#    provider    => 'pip',
+#    upgradeable => true,
+#  }
+  #$ubuntu_packages = [ 'python','PyYAML','libtomcrypt','libtommath','libyaml','python-babel','python-httplib2','python-jinja2','python-keyczar','python-markupsafe','python-six','python2-crypto','python2-ecdsa','python2-paramiko','gcc', 'gmp-devel','sshpass', 'python2-pyasn1','python34','python-pip']
+  $ubuntu_packages = ['python','python3.5','python-pip']
+  exec {'/usr/bin/apt update':}
+  -> package { $ubuntu_packages:
     ensure => 'latest',
   }
   -> package { 'ansible':
-    ensure   => 'present',
-    provider => 'pip',
+    ensure      => 'present',
+    provider    => 'pip',
+    #upgradeable => true,
   }
-}

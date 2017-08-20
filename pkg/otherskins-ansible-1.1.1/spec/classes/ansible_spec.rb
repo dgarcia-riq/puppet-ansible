b8add913751d10f58c8c66b0f5257f53b2571d00
Edgar Silva <edgarsilva948@gmail.com>
2017-08-20 04:08:47 -0300

    Include .gitattributes file

---
+++ pkg/otherskins-ansible-1.1.1/spec/classes/ansible_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe 'ansible', :type => :class do
+
+  it { is_expected.to compile.with_all_deps }
+# it { is_expected.to contain_class('ansible::install').that_comes_before('Class[ansible::config]') }
+# it { is_expected.to contain_class ('ansible::config')}
+
+end
---
+++ pkg/otherskins-ansible-1.1.1/spec/defines/hosts_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe 'ansible::hosts' do
+    let(:title) { 'webservers' }
+end

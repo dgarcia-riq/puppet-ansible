5ff0ae1b5cfbe1912c9951e909809d6aba7d89a3
Vinicius Xavier <viniciusxavierbove@gmail.com>
2017-08-23 01:05:28 -0300

    Include new examples in Rspec unit test

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,12 +3,13 @@ class ansible::install {
 
   $packages = lookup('ansible::packages')
 
-    package { $packages:
-      ensure => $ansible::version,
-    }
+  package { $packages:
+    ensure => $ansible::version,
+  }
 
   -> package { $ansible::package_name:
     ensure   => $ansible::version,
     provider => $ansible::provider,
   }
+
 }
--- spec/defines/hosts_spec.rb
+++ spec/defines/hosts_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe 'ansible::hosts' do
+
     let(:title) { 'webservers' }
+
+    let(:params) { {
+        entrys => [
+          '192.168.0.1',
+          '192.168.0.2'
+        ]
+    } }
 end

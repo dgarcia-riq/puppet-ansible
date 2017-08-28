9203a9fc45043fcf0d7bf5f112877c22a9b86417
Edgar Silva <edgarsilva948@gmail.com>
2017-08-28 01:35:18 -0300

    Fix beaker tests with centos7

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,7 +1,17 @@
-require 'beaker-rspec/spec_helper'
-require 'beaker-rspec/helpers/serverspec'
+#require 'beaker-rspec/spec_helper'
+#require 'beaker-rspec/helpers/serverspec'
+require 'beaker-rspec'
+require 'beaker/puppet_install_helper'
+require 'beaker/module_install_helper'
 
 PROJECT_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+
+UNSUPPORTED_PLATFORMS = ['windows', 'Darwin']
+
+run_puppet_install_helper
+install_ca_certs unless ENV['PUPPET_INSTALL_TYPE'] =~ /pe/i
+install_module_on(hosts)
+install_module_dependencies_on(hosts)
 
 RSpec.configure do |c|
   c.before :suite do

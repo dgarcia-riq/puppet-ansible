3b1497c353f21cf2198d37c1fc0b287f3ac53dad
Edgar Silva <edgarsilva948@gmail.com>
2017-08-28 02:33:28 -0300

    Merge tests files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,20 +1,4 @@
 require 'rspec-puppet'
-require 'puppetlabs_spec_helper/module_spec_helper'
-
-
-if Puppet::Util::Package.versioncmp(Puppet.version, '4.5.0') >= 0
-  RSpec.configure do |c|
-    c.before :each do
-      Puppet.settings[:strict] = :error
-    end
-  end
-end
-
-# put local configuration and setup into spec_helper_local
-begin
-  require 'spec_helper_local'
-rescue LoadError
-end
 
 fixture_path = File.join(File.dirname(File.expand_path(__FILE__)), 'fixtures')
 

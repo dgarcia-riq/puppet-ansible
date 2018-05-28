ce12701a9cba66f656bc968d8ccbcda7f56ee57f
Paweł Kalemba <5924586+pkalemba@users.noreply.github.com>
2018-05-28 13:10:58 +0200

    Support debian 9
    Apt module installs dirmngr by default on debian 9

--- manifests/repo/apt.pp
+++ manifests/repo/apt.pp
@@ -1,21 +1,18 @@
 # PRIVATE CLASS: do not use directly
 class ansible::repo::apt {
 
-  contain ::apt
+  include ::apt
 
   apt::source { 'ansible_repo':
     ensure   => $ansible::ensure,
     comment  => 'This is the latest ansible debian/ubuntu mirror',
     location => 'http://ppa.launchpad.net/ansible/ansible/ubuntu',
-    release  => 'trusty',
+    release  => 'bionic',
     repos    => 'main',
     key      => {
-      'id'     => '93C4A3FD7BB9C367',
+      'id'     => '6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367',
       'server' => 'hkp://keyserver.ubuntu.com:80',
     },
     include  => { 'deb' => true },
   }
-
-    Apt::Source['ansible_repo']
-    ~> Exec['apt_update']
 }
